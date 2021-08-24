b5994af77126d19fe3a7afd14bebf4824a628d86
tenclar <tenclarvalus@gmail.com>
2021-08-24 01:44:52 -0500

    resource

--- hc-api/src/main/java/com/hc/api/resource/CustomerResource.java
+++ hc-api/src/main/java/com/hc/api/resource/CustomerResource.java
@@ -1,10 +1,20 @@
 package com.hc.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import com.hc.api.model.Customer;
 import com.hc.api.repository.CustomerRepository;
 
@@ -19,6 +29,20 @@ public class CustomerResource {
 	@GetMapping
 	public List<Customer> list(){
 		return customerRepository.findAll();
+	}
+	
+	@PostMapping
+	// @ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Customer> create(@RequestBody Customer customer, HttpServletResponse response) {
+		
+	  Customer customerSave = customerRepository.save(customer);
+	  
+	  URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/id")
+	  .buildAndExpand(customerSave.getId()).toUri();
+	  response.setHeader("Location",uri.toASCIIString());
+	  
+	 	 
+	  return ResponseEntity.created(uri).body(customerSave);
 	}
 	
 }
