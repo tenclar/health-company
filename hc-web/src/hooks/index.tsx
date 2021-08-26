f10accaef83b9363c10f08f55715befaf0e7a5d9
tenclar <tenclarvalus@gmail.com>
2021-08-26 00:43:00 -0500

    Componente toast

---
+++ hc-web/src/hooks/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+import { ToastProvider } from './Toast';
+
+const AppProvider: React.FC = ({ children }) => (
+
+    <ToastProvider>{children}</ToastProvider>
+
+);
+export default AppProvider;
